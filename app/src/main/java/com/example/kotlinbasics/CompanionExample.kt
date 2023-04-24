d33295c61d286111069e8b8d31b94c676bcb1159
yubinkarki <iamyubinkarki@gmail.com>
2023-04-24 17:09:30 +0545

    Companion example

---
+++ app/src/main/java/com/example/kotlinbasics/CompanionExample.kt
@@ -0,0 +1,14 @@
+package com.example.kotlinbasics
+
+fun main() {
+    Animal.takePlate()
+}
+
+class Animal {
+    // Companion objects can be accessed directly without creating instance of class.
+    companion object Action {
+        fun takePlate() {
+            println("This is take plate method inside food.")
+        }
+    }
+}
