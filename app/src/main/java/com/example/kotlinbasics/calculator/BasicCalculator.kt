cec92aa18806d6301956a73ac3302a24fc429279
yubinkarki <iamyubinkarki@gmail.com>
2023-06-19 18:41:04 +0545

    Basic calculator activity

---
+++ app/src/main/java/com/example/kotlinbasics/calculator/BasicCalculator.kt
@@ -0,0 +1,16 @@
+package com.example.kotlinbasics.calculator
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.kotlinbasics.R
+
+class BasicCalculator : AppCompatActivity() {
+    override fun onCreate(b: Bundle?) {
+        super.onCreate(b)
+        setContentView(R.layout.basic_calculator)
+    }
+
+    fun onDigit() {
+
+    }
+}
